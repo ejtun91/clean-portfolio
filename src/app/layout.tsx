19cbdde1df625fa4b2f616451be39d52cbf1286a
Antonio K (ejtun91) <ejtun91@gmail.com>
2023-08-30 08:25:14 +0200

    export metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
@@ -6,7 +5,6 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import LinksLeft from "./components/LinksLeft";
 import LinksRight from "./components/LinksRight";
-import { useEffect, useState } from "react";
 import SplashScreen from "./components/SplashScreen";
 
 const poppins = Poppins({
@@ -26,38 +24,9 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [disableSpinner, setDisableSpinner] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setDisableSpinner(false);
-    }, 2000);
-  }, []);
-
   return (
     <html lang="en">
-      <body className={poppins.className}>
-        {disableSpinner ? (
-          <SplashScreen />
-        ) : (
-          <>
-            {" "}
-            <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-            <LinksLeft />
-            <main
-              onClick={() => setMenuOpen(false)}
-              className={`${
-                menuOpen ? "z-30 relative blur-sm md:blur-none" : ""
-              } `}
-            >
-              {children}
-            </main>{" "}
-            <LinksRight />
-            <Footer />
-          </>
-        )}
-      </body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,51 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Jumbotron from "./components/Jumbotron";
 import Projects from "./components/Projects";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import SplashScreen from "./components/SplashScreen";
+import Navbar from "./components/Navbar";
+import LinksLeft from "./components/LinksLeft";
+import LinksRight from "./components/LinksRight";
+import Footer from "./components/Footer";
 
 export default function Home() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [disableSpinner, setDisableSpinner] = useState(true);
   useEffect(() => {
+    setTimeout(() => {
+      setDisableSpinner(false);
+    }, 2000);
     AOS.init();
   }, []);
 
   return (
-    <section>
-      <Jumbotron />
-      <About />
-      <Projects />
-      <Contact />
-    </section>
+    <>
+      {disableSpinner ? (
+        <SplashScreen />
+      ) : (
+        <>
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <LinksLeft />
+          <main
+            onClick={() => setMenuOpen(false)}
+            className={`${
+              menuOpen ? "z-30 relative blur-sm md:blur-none" : ""
+            } `}
+          >
+            {" "}
+            <Jumbotron />
+            <About />
+            <Projects />
+            <Contact />{" "}
+          </main>{" "}
+          <LinksRight />
+          <Footer />
+        </>
+      )}
+    </>
   );
 }
